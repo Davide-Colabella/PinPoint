1f3f7cd2160e74ce0a6119b709d9302aeeaee6f6
Davide Colabella <s1098071@studenti.univpm.it>
2023-05-05 01:36:40 +0200

    Fix previous commit

---
+++ app/src/main/java/com/univpm/pinpointmvvm/view/activities/AccountEditActivity.kt
@@ -0,0 +1,80 @@
+package com.univpm.pinpointmvvm.view.activities
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.univpm.pinpointmvvm.R
+import com.univpm.pinpointmvvm.databinding.ActivityAccountEditBinding
+import com.univpm.pinpointmvvm.viewmodel.ProfileViewModel
+import kotlinx.coroutines.launch
+
+class AccountEditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAccountEditBinding
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAccountEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ProfileViewModel()
+        var imageUri: Uri? = null
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
+                imageUri = uri
+                binding.profileImageAccountSettings.load(imageUri) {
+                    crossfade(true)
+                    placeholder(R.drawable.ic_profile)
+                    transformations(CircleCropTransformation())
+                }
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
+        binding.saveAccountSettingsBtn.setOnClickListener {
+            if (imageUri != null) {
+                viewModel.updateProfile(
+                    binding.accountNameAccountSettings.text.toString(),
+                    binding.accountUsernameAccountSettings.text.toString(),
+                    binding.accountBioAccountSettings.text.toString(),
+                    imageUri!!
+                )
+            } else {
+                viewModel.updateProfile(
+                    binding.accountUsernameAccountSettings.text.toString(),
+                    binding.accountNameAccountSettings.text.toString(),
+                    binding.accountBioAccountSettings.text.toString()
+                )
+            }
+            finish()
+        }
+
+        binding.closeAccountSettingsBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.changeProfileImage.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect { uiState ->
+                binding.accountUsernameAccountSettings.setText(uiState.username)
+                binding.accountNameAccountSettings.setText(uiState.fullname)
+                binding.accountBioAccountSettings.setText(uiState.bio)
+                binding.profileImageAccountSettings.load(uiState.image) {
+                    placeholder(R.drawable.ic_profile)
+                    error(R.drawable.ic_profile)
+                    transformations(CircleCropTransformation())
+                }
+            }
+        }
+    }
+}
