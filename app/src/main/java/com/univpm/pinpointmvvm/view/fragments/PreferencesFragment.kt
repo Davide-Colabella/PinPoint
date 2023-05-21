70f538453cda2d1e11fa33d5dceb6bc9d1bdfad3
Matteo <s1098185@studenti.univpm.it>
2023-05-21 10:18:02 +0200

    preferences fragment

---
+++ app/src/main/java/com/univpm/pinpointmvvm/view/fragments/PreferencesFragment.kt
@@ -0,0 +1,43 @@
+package com.univpm.pinpointmvvm.view.fragments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.univpm.pinpointmvvm.R
+
+class PreferencesFragment : PreferenceFragmentCompat() {
+
+    companion object {
+        const val LOCATION_KEY = "localization_preference"
+        const val DARK_THEME_KEY = "list_preference_1"
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val darkThemePreference = findPreference<ListPreference>(DARK_THEME_KEY)
+        darkThemePreference?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+
+            val selectedValue = newValue.toString()
+            applyDarkMode(selectedValue)
+
+            true // Restituisci true per indicare che hai gestito il cambiamento di preferenza
+        }
+    }
+
+
+
+    private fun applyDarkMode(selectedValue: String) {
+        val nightMode = when (selectedValue) {
+            "0" -> AppCompatDelegate.MODE_NIGHT_NO
+            "1" -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+    }
+
+
+
+}
